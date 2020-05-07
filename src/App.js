4ce1a032c29ff4a872cb89cbac73f3a34af9ff50
rahmasetiawan500 <rahmasetiawan500@gmail.com>
2020-05-07 16:12:26 +0700

    design Web

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const userLogged = "Zuma";
+  // const userLoggedJSX = <strong> Menuju Masa Depan</strong>;
+  // let a = 9;
+  // let b = 3;
+  const category = "INDONESIA";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parentbox">
+
+      <div className="Foto">
+      <img src="popular1.png" />
+
+      </div>
+
+      <div className="Deskripsi">
+      <p className="Cate">{category}</p>
+      <h1 className="Title">Ubud, Bali</h1>
+      <p className="Price">IDR 25.678.677</p>
+      <p className="Info">
+      Aliqua et laboris adipisicing adipisicing sint est adipisicing tempor. Aliqua do in nulla ullamco proident. Nisi voluptate qui sunt elit eu pariatur sint sint ex ipsum. Labore sint ullamco officia commodo est. Voluptate eu et voluptate pariatur in. Lorem duis aliqua aliquip incididunt cillum amet.
+      </p>
+      </div>
+
+     { 
+     /* 
+     a + b / 1 
+     {userLogged}
+     {userLoggedJSX} */
+     }
+
     </div>
   );
 }
