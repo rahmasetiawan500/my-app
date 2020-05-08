68aa24ff6d0330398935a66f0a5b0d87dd5212bb
rahmasetiawan500 <rahmasetiawan500@gmail.com>
2020-05-08 13:23:29 +0700

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="Parentbox">
 
     <FotoProduk />
-    <ProdukInfo name= "Ubud, Bali" category = "Indonesia" />
+    <ProdukInfo isDiscount = "comming" name= "Ubud, Bali" category = "Indonesia" />
 
      { 
      /* 
@@ -36,15 +36,33 @@ function FotoProduk() {
 
 }
 
+function CheckDiscount(props) {
+  const { isDiscount }= props;
+if (isDiscount == "yes") {
+  return (
+    <p>Diskon 50% dapatkan sekarang</p>
+  );
+}  else if (isDiscount == "comming") {
+  return(
+    <p>Diskon SEGERA </p>
+  );
+} else {
+return (
+    <p>Tidak Ada Diskon</p>
+    );
+}
+  
+}
 
 function ProdukInfo(props) {
-  const {category, name} = props ;
+  const {category, name, isDiscount} = props ;
   return (
     
     <div className="Deskripsi">
     <p className="Cate">{category}</p>
     <h1 className="Title">{name}</h1>
     <p className="Price">IDR 25.678.677</p>
+    <CheckDiscount isDiscount={isDiscount} />
     <p className="Info">
     Aliqua et laboris adipisicing adipisicing sint est adipisicing tempor. Aliqua do in nulla ullamco proident. Nisi voluptate qui sunt elit eu pariatur sint sint ex ipsum. Labore sint ullamco officia commodo est. Voluptate eu et voluptate pariatur in. Lorem duis aliqua aliquip incididunt cillum amet.
     </p>
