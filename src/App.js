7b3d45fe7c1df5766504d1a946a14e81d73b5d18
rahmasetiawan500 <rahmasetiawan500@gmail.com>
2020-05-08 12:31:03 +0700

    slicingdesign

--- src/App.js
+++ src/App.js
@@ -8,23 +8,12 @@ function App() {
   // const userLoggedJSX = <strong> Menuju Masa Depan</strong>;
   // let a = 9;
   // let b = 3;
-  const category = "INDONESIA";
+
   return (
     <div className="Parentbox">
 
-      <div className="Foto">
-      <img src="popular1.png" />
-
-      </div>
-
-      <div className="Deskripsi">
-      <p className="Cate">{category}</p>
-      <h1 className="Title">Ubud, Bali</h1>
-      <p className="Price">IDR 25.678.677</p>
-      <p className="Info">
-      Aliqua et laboris adipisicing adipisicing sint est adipisicing tempor. Aliqua do in nulla ullamco proident. Nisi voluptate qui sunt elit eu pariatur sint sint ex ipsum. Labore sint ullamco officia commodo est. Voluptate eu et voluptate pariatur in. Lorem duis aliqua aliquip incididunt cillum amet.
-      </p>
-      </div>
+    <FotoProduk />
+    <ProdukInfo />
 
      { 
      /* 
@@ -32,9 +21,37 @@ function App() {
      {userLogged}
      {userLoggedJSX} */
      }
-
     </div>
   );
+}
+
+
+function FotoProduk() {
+  return (
+  <div className="Foto">
+  <img src="popular1.png" />
+  </div>
+
+  );
+
+}
+
+
+function ProdukInfo() {
+  const category = "INDONESIA";
+  return (
+    
+    <div className="Deskripsi">
+    <p className="Cate">{category}</p>
+    <h1 className="Title">Ubud, Bali</h1>
+    <p className="Price">IDR 25.678.677</p>
+    <p className="Info">
+    Aliqua et laboris adipisicing adipisicing sint est adipisicing tempor. Aliqua do in nulla ullamco proident. Nisi voluptate qui sunt elit eu pariatur sint sint ex ipsum. Labore sint ullamco officia commodo est. Voluptate eu et voluptate pariatur in. Lorem duis aliqua aliquip incididunt cillum amet.
+    </p>
+    </div>
+
+      );
+  
 }
 
 export default App;
